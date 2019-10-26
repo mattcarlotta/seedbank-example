ff6ae8df461f73ece1660eac03c88f2d7cf46816
Matt <carlotta.matt@gmail.com>
2019-10-26 16:18:05 -0700

    Add more notes

--- client/src/helpers/axiosConfig.js
+++ client/src/helpers/axiosConfig.js
@@ -1,14 +1,19 @@
 import get from "lodash/get";
 import axios from "axios";
-// import { inDevelopment, APIPORT } from "../../../config/envs";
 
+// the baseURL should to point to localhost in development
+// and your domain in production
 const app = axios.create({
-  // baseURL: inDevelopment
-  //   ? `http://localhost:${APIPORT}/api/`
-  //   : "https://example.com",
+  // baseURL: process.env.NODE_ENV === inDevelopment
+  //   ? `http://localhost:5000/api/`
+  //   : "http://example.com",
   baseURL: "http://localhost:5000/api/"
 });
 
+// axios consumes rejected API responses by default,
+// so the configuration below intercepts the those
+// responses and passes them down to the function that
+// uses our custom "app" axios configuration.
 app.interceptors.response.use(
   response => response,
   error => {
@@ -17,7 +22,7 @@ app.interceptors.response.use(
     // "err" refers to the response returned from the API when
     // a response is rejected. "err" can be named anything, but it
     // must be consistent across all your API routes (for example, see
-    // anarchive/routes/api => verify route )
+    // anarchive/routes/api => verify route => "catch" sends the "err")
     return Promise.reject(err ? err : error.message);
   }
 );
